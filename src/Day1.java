8af523ce96f09c1162aa780402e6a3c6d91f2878
Pavlo Petrenko <ppetrenko@wayfair.com>
2022-12-01 12:09:56 +0100

    initial commit

---
+++ src/Day1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Day1 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    var maxHeap = new PriorityQueue<Integer>((a,b) -> Integer.compare(b, a));
+    int current = 0;
+    String line;
+    while ((line = reader.readLine()) != null) {
+      if (line.isEmpty()) {
+        maxHeap.add(current);
+        current = 0;
+      } else {
+        current += Long.parseLong(line);
+      }
+    }
+    maxHeap.add(current);
+    int result = 0;
+    for (int i = 0; i < 3; i++) {
+      result += maxHeap.poll();
+    }
+    System.out.println(result);
+  }
+}
