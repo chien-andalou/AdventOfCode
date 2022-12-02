f1b6c0ab0970635b2796871c35cbf288bf718e85
Pavlo Petrenko <ppetrenko@wayfair.com>
2022-12-02 10:41:44 +0100

    Refactor

--- src/Day1.java
+++ src/Day1.java
@@ -5,29 +5,31 @@ import java.util.PriorityQueue;
 
 public class Day1 {
   public static void main(String[] args) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     var minHeap = new PriorityQueue<Integer>();
     int current = 0;
-    String line;
-    while ((line = reader.readLine()) != null) {
-      if (line.isEmpty()) {
-        minHeap.add(current);
-        if (minHeap.size() > 3) {
-          minHeap.poll();
+    try (var reader = new BufferedReader(new InputStreamReader(System.in))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        if (line.isEmpty()) {
+          addToHeap(minHeap, current);
+          current = 0;
+        } else {
+          current += Integer.parseInt(line);
         }
-        current = 0;
-      } else {
-        current += Integer.parseInt(line);
       }
     }
-    minHeap.add(current);
-    if (minHeap.size() > 3) {
-      minHeap.poll();
-    }
+    addToHeap(minHeap, current);
     int result = 0;
     while (!minHeap.isEmpty()) {
       result += minHeap.poll();
     }
     System.out.println(result);
+  }
+
+  private static void addToHeap(PriorityQueue<Integer> minHeap, int current) {
+    minHeap.add(current);
+    if (minHeap.size() > 3) {
+      minHeap.poll();
+    }
   }
 }
